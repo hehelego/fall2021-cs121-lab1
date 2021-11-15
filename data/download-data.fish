c1430e8f02834c207874911b77b1b2a8657d6086
hehelego <2364261262@qq.com>
2021-11-15 23:25:51 +0800

    add(download_data.fish): a fish shell script to download the first four testcases

---
+++ data/download-data.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/fish
+aria2c -j6 https://snap.stanford.edu/data/roadNet-CA.txt.gz
+aria2c -j6 https://snap.stanford.edu/data/bigdata/communities/com-orkut.ungraph.txt.gz
+aria2c -j6 https://snap.stanford.edu/data/soc-LiveJournal1.txt.gz
+aria2c -j6 https://snap.stanford.edu/data/web-Stanford.txt.gz
