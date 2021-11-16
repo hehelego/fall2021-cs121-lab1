4e0b3628772bc34ec5ffddf7b74f135a4010b35c
hehelego <2364261262@qq.com>
2021-11-16 15:49:52 +0800

    upd(data/download.fish): decompress fetched gzip files

--- data/download-data.fish
+++ data/download-data.fish
@@ -1,5 +1,15 @@
 #!/usr/bin/fish
+
+
+# fetch the first 4 testing dataset from snap,
+# aria2 is employed for parallelzed downloading
 aria2c -j6 https://snap.stanford.edu/data/roadNet-CA.txt.gz
 aria2c -j6 https://snap.stanford.edu/data/bigdata/communities/com-orkut.ungraph.txt.gz
 aria2c -j6 https://snap.stanford.edu/data/soc-LiveJournal1.txt.gz
 aria2c -j6 https://snap.stanford.edu/data/web-Stanford.txt.gz
+
+# decompress
+set files (ls | rg '.gz$')
+for f in $files
+	gzip --keep --decompress $f
+end
