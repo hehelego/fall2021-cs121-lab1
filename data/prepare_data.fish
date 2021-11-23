63ab0004dc34bf505a950097c562483339010c58
hehelego <2364261262@qq.com>
2021-11-23 13:20:05 +0800

    upd(data/prepare.fish): add conditional-get option for aria2 downloader

--- data/prepare_data.fish
+++ data/prepare_data.fish
@@ -3,10 +3,13 @@
 
 # fetch the first 4 testing dataset from snap,
 # aria2 is employed for parallelzed downloading
-aria2c -j6 https://snap.stanford.edu/data/roadNet-CA.txt.gz
-aria2c -j6 https://snap.stanford.edu/data/bigdata/communities/com-orkut.ungraph.txt.gz
-aria2c -j6 https://snap.stanford.edu/data/soc-LiveJournal1.txt.gz
-aria2c -j6 https://snap.stanford.edu/data/web-Stanford.txt.gz
+function download
+	aria2c -j6 --allow-overwrite --conditional-get $argv
+end
+download https://snap.stanford.edu/data/roadNet-CA.txt.gz
+download https://snap.stanford.edu/data/bigdata/communities/com-orkut.ungraph.txt.gz
+download https://snap.stanford.edu/data/soc-LiveJournal1.txt.gz
+download https://snap.stanford.edu/data/web-Stanford.txt.gz
 
 # decompress
 set files (ls | grep '.gz$')
