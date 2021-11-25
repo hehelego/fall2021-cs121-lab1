26734438e66d31c5f7430f996822973d17d06e32
hehelego <2364261262@qq.com>
2021-11-25 08:48:00 +0800

    fix(data/gen-RMAT.fish): generate undirected graph

--- data/gen-RMAT.fish
+++ data/gen-RMAT.fish
@@ -8,6 +8,6 @@ cd ../../
 
 set n 100000000
 set m 1000000000
-./PaRMAT/Release/PaRMAT -nVertices $n -nEdges $m -undirected -thread 32 -noDuplicateEdges -a 0.3  -b 0.25 -c 0.25 -output RMAT1.txt
-./PaRMAT/Release/PaRMAT -nVertices $n -nEdges $m -undirected -thread 32 -noDuplicateEdges -a 0.45 -b 0.25 -c 0.15 -output RMAT2.txt
-./PaRMAT/Release/PaRMAT -nVertices $n -nEdges $m -undirected -thread 32 -noDuplicateEdges -a 0.57 -b 0.19 -c 0.19 -output RMAT3.txt
+./PaRMAT/Release/PaRMAT -nVertices $n -nEdges $m -thread 32 -noDuplicateEdges -a 0.3  -b 0.25 -c 0.25 -output RMAT1.txt
+./PaRMAT/Release/PaRMAT -nVertices $n -nEdges $m -thread 32 -noDuplicateEdges -a 0.45 -b 0.25 -c 0.15 -output RMAT2.txt
+./PaRMAT/Release/PaRMAT -nVertices $n -nEdges $m -thread 32 -noDuplicateEdges -a 0.57 -b 0.19 -c 0.19 -output RMAT3.txt
